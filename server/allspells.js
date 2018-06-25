06ebc32000af8dd13a71f2a35b286fbe8ace922b
Alex Hunter <alexander.hunter.80@gmail.com>
2018-06-25 14:22:18 -0700

    reinit to fix weird bindings

--- server/allspells.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { name: 'Quickness', text: 'AP +2', elements: 'AA', effect: {type: 'AP_PLUS', value: 2}, targeted: false, copies: 4 },
-    { name: 'Stoneskin', text: 'Shield 1', elements: 'EE', effect: {type: 'CURE', value: 1}, targeted: false, copies: 4 },
-    { name: 'Fireball', }
-]
--- server/classes/element.js
+++
@@ -1,9 +0,0 @@
-module.exports = class ElementCard {
-    constructor(elem){
-        this.elem = elem;
-        this.imagepath = null; // may not need this
-        this.faceUp = false;
-    }
-
-    // method(){}
-}
